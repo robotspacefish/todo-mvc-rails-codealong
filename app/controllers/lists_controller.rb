b994ebab334a0839b46ebbad6887f6f6482bb6c8
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-27 17:38:32 -0500

    create a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   def index
+    @list = List.new
     @lists = List.all
     # raise @lists.inspect
   end
@@ -7,4 +8,15 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+    def list_params # strong params
+      params.require(:list).permit(:name)
+    end
 end
